8df04c5ee57f69d222f1deffda56c876f8e693ae
saskakrosnjar <saska.krosnjar@gmail.com>
2023-03-12 20:09:38 +0100

    Importing Link and changing name of styles.css

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+
+function Navbar() {
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+  return (
+    <header>
+      <h3>Logo</h3>
+      <nav ref={navRef}>
+        <Link to="/">Home</Link>
+        <Link to="/str1">Str1</Link>
+        <Link to="/str2">Str2</Link>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+}
+
+export default Navbar;
